4fecc6411aa64de25a85afc3bfb1475d9d0890f4
Ahnaf Shahriar <bdahnaf@gmail.com>
2026-04-07 18:28:06 +0600

    Added DI for WakaTime

--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+// Register WakaTimeService for dependency injection
+builder.Services.AddHttpClient<Portfolio.Services.WakaTimeService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
